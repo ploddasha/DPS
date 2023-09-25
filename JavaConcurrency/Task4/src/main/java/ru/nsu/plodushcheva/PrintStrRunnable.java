f5b0e917873d7bff93f2ea622d29204e6a6cc1fb
ploddasha <ploddasha@yandex.ru>
2023-09-25 16:30:50 +0700

    added task 4

---
+++ JavaConcurrency/Task4/src/main/java/ru/nsu/plodushcheva/PrintStrRunnable.java
@@ -0,0 +1,16 @@
+package ru.nsu.plodushcheva;
+
+public class PrintStrRunnable implements Runnable{
+    private final String str;
+
+    public PrintStrRunnable(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.println(str);
+        }
+    }
+}
