291a7971886edecaac8c43c9d65512bf67b62530
ploddasha <ploddasha@yandex.ru>
2023-12-18 22:13:12 +0700

    modified test a bit

--- Clojure/task3/test/task3/core_test.clj
+++ Clojure/task3/test/task3/core_test.clj
@@ -4,7 +4,17 @@
 
 (deftest test-p-filter
   (is (= (take 10 (p-filter heavy-even? (range 100))) (range 0 20 2)))
+  )
+
+(deftest test-lazy
   (is (= (take 10 (p-filter heavy-even? (iterate inc 2))) (range 2 21 2)))
+  )
+
+(deftest test-filter-equal
   (is (= (take 10 (p-filter heavy-even? (range 0))) (filter even? (range 0))))
   (is (= (p-filter even? (range 10000)) (filter even? (range 10000))))
+  )
+
+(deftest bigger-p-filter
+  (is (= (p-filter even? (range 10000000)) (filter even? (range 10000000))))
   )
